d324741ac067deba0d6dc32214f9ea55d624beed
BrentAlbers <93827410+BrentAlbers@users.noreply.github.com>
2023-03-16 10:17:39 +0100

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,78 @@
+@vite('resources/css/app.css')
+<footer class="bg-gray-800 py-20 ">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700 ">
+        <div class="">
+            <h3 class="text-l sm:font-bold text-gray-100"> Pages</h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        lorem
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- <div>
+            <h3 class="text-l sm:font-bold text-gray-100">Find us</h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Adress
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Phone
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        contact
+                    </a>
+                </li>
+            </ul>
+        </div> -->
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                lorem
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        N/A
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    N/A
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    N/A
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                    N/A
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        copyright
+    </p>
+</footer>
