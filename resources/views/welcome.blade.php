eb0e3d092a8753ee255b657c07ae727755b96224
BrentAlbers <93827410+BrentAlbers@users.noreply.github.com>
2023-03-16 10:15:32 +0100

    find out more button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,10 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class=" sm:text-white text-5x1 uppercase font-bold text-shadow-md pb-14">Vaan vitaliteit</h1>
-
+                <a href="/" class="text-center bg-gray-50 text-gray-700 
+                py-2 px-4 font-bold text-xl uppercase">
+                    Find out more
+                </a>
             </div>
         </div>
     </div>
