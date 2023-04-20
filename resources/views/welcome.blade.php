d7cf5c9dbbe7aafa9414cafc29400be27bd796bd
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-04-20 13:12:04 +0200

    homescrreen gemaakt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
 	
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet">
 
@@ -19,13 +21,16 @@
         #menu-toggle:checked + #menu {
             display: block;
         }
-        body header{
-            background: rgb(248, 242, 231);
+        body{
+            background: #F7EFDF;
         }
         
         .hover\:grow {
             transition: all 0.3s;
             transform: scale(1);
+        }
+        #header{
+            background-color: #F7EFDF;
         }
         
         .hover\:grow:hover {
@@ -84,29 +89,49 @@
         .homeimg{
             margin-top: 900px;
         }
-        #headerScroll {
-            background-color: transparent; 
-            padding: 15px; 
-            color: black;
+        .waveline{
+            margin-top: 630px;
+        }
+        #headerScrollbg{
+            background-color: #2A9D8F; 
+            padding: 13px; 
+            color: transparent;
             text-align: center; 
             font-size: 30px;
-            font-weight: bold;
+            font-family: 'Work Sans', sans-serif;
+            font-weight: 400;
             position: fixed; 
             width: 100%; 
             top: 0;
-            z-index: 1;
-            animation: up 450ms linear ;
+            animation: up 280ms linear ;
+            animation-play-state: paused;
+            animation-delay: calc(var(--scroll) * -1s);
+        }
+        #headerScroll {
+            background-color: transparent; 
+            padding: 13px; 
+            color: black;
+            text-align: center; 
+            font-size: 30px;
+            font-family: 'Work Sans', sans-serif;
+            font-weight: 400;
+            position: fixed; 
+            width: 100%; 
+            z-index: 30;
+            top: 0;
+            animation: up 280ms linear ;
             animation-play-state: paused;
             animation-delay: calc(var(--scroll) * -1s);
             }
             @keyframes up {
             from {
                 font-size: 90px;
-                padding: 350px 20px;
+                padding: 400px 20px;
             }
             to {
                 font-size: 30px;
                 padding: 15px;
+
             }
             }
     </style>
@@ -121,7 +146,7 @@
 <body class="text-gray-600 work-sans leading-normal text-base tracking-normal">
 
     <!--Nav-->
-    <nav id="header" class="w-full z-30 top-0 sticky bg-white py-1">
+    <nav id="header" class="w-full z-30 top-0 sticky bg py-1">
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
 
             <label for="menu-toggle" class="cursor-pointer md:hidden block">
@@ -132,7 +157,7 @@
             </label>
             <input class="hidden" type="checkbox" id="menu-toggle" />
 
-            <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
+            <div class="hidden z-50 md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
                 <nav>
                     <ul class="md:flex items-center justify-between text-base text-header-menu font-bold pt-4 md:pt-0">
                         <li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">Subject 1</a></li>
@@ -144,14 +169,14 @@
                 </nav>
             </div>
 
-            <div class="order-1 md:order-2">
+            <div class="order-1 z-10 md:order-2">
                 <a class="flex items-left tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
 
                     <!-- <h1 class="primary-text-color items-center">De Vaan Vitaliteit</h1> -->
                 </a>
             </div>
 
-            <div class="order-2 md:order-3 flex items-center" id="nav-content">
+            <div class="order-2 md:order-3 flex items-center z-10 " id="nav-content">
 
                 <a class="inline-block no-underline hover:text-black" href="#">
                     <svg class="fill-current hover:text-black primary-text-color" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
@@ -163,10 +188,34 @@
             </div>
         </div>
         <div id="headerScroll">
-            De Vaan Vitaliteit
+            <h1 class="">De Vaan Vitaliteit</h1>
         </div>
+
         
     </nav>
+ 
+        <div id="headerScrollbg">
+            <h1 class="">De Vaan Vitaliteit</h1>
+        </div>
+    <div class="waveline  relative z-10">
+      <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+            <path
+              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+              opacity="0.100000001"
+            ></path>
+            <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
+          </g>
+          <g transform="translate(-4.000000, 76.000000)" fill="#F7EFDF" fill-rule="nonzero">
+            <path
+              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    </div>
     <div class=" homeimg flex h-screen w-full justify-center items-center ">
         <img src="/images/Ribalta_Espaco_1.jpg" alt="">
     </div>
