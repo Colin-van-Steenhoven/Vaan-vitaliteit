99a07677cc7940ad58060cf76707ad33cb40bb80
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-05-31 13:47:58 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -443,6 +443,8 @@
         margin-top: 20px;
         height: 45px;
         }
+        .priceplan{
+        }
 
 
     </style>
@@ -592,34 +594,90 @@
 
 <!--paketten-->
 
-<section id="features">
-    <div class="feature-row">
-        <div class="feature-col">
-            <img src="" alt="">
-            <i class="fa fa-duotone fa-house"></i>
-            <i class="fa fa-brands fa-twitter"></i>
-            <div class="title-border">
-                <h4>Pakket 1</h4>
-            </div>
-            <h2 class="hr-lines">PEACE</h2>
-            <p>Lorem ipsum dolor sit amet consectetur,tibus culpa harum ut itaque, at dolorem distinctio quos accusantium incidunt eligendi, obcaecati aliquid debitis soluta?</p>
-        </div>
-        <div class="feature-col">
-            <img src="" alt="">
-            <div class="title-border">
-                <h4>Pakket 1</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur,tibus culpa harum ut itaque, at dolorem distinctio quos accusantium incidunt eligendi, obcaecati aliquid debitis soluta?</p>
-        </div>
-        <div class="feature-col">
-            <img src="" alt="">
-            <div class="title-border">
-                <h4>Pakket 1</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur,tibus culpa harum ut itaque, at dolorem distinctio quos accusantium incidunt eligendi, obcaecati aliquid debitis soluta?</p>
-        </div>
+
+    <style>
+    [x-cloak] {
+    display: none;
+    }
+</style>
+
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
+
+<div class="container mx-auto antialiased text-gray-900 bg-white" x-data="setup()">
+  <!-- Navbar -->
+
+
+  <main class="mx-4 my-16">
+    <div class="text-center">
+      <h1 class="mb-4 text-2xl font-normal md:text-3xl lg:text-4xl">
+        Lorem <span class="font-semibold">ipsum</span> dolor sit <span class="font-semibold">amet</span>
+      </h1>
+      <p class="text-sm font-normal text-gray-400">
+      Hic a facilis vero eos omnis culpa quibusdam, ex vitae ipsum quasi
+      </p>
+      <p class="text-sm font-normal text-gray-400">
+      voluptate excepturi, necessitatibus nisi similique.
+      </p>
     </div>
-</section>
+
+    <!-- Plan switch -->
+
+
+    <!-- Plans -->
+    <div
+         class="flex flex-col items-center justify-center mt-16 space-y-8 lg:flex-row lg:items-stretch lg:space-x-8 lg:space-y-0"
+         >
+      <template x-for="(plan, i) in plans" x-key="i">
+        <section class="priceplan flex flex-col w-full max-w-sm p-12 space-y-6  rounded-lg shadow-md">
+          <!-- Price -->
+          <div class="flex-shrink-0">
+            <span
+                  class="text-4xl font-medium tracking-tight"
+                  :class="plan.name == 'Basic' ? 'text-green-500' : ''"
+                  x-text="plan.price"
+                  ></span>
+            <span class="text-gray-400" x-text="billPlan == 'monthly' ? '/month' : ' prijs'"></span>
+          </div>
+
+          <!--  -->
+          <div class="flex-shrink-0 pb-6 space-y-2 border-b">
+            <h2 class="text-2xl font-normal" x-text="plan.name"></h2>
+            <p class="text-sm text-gray-400" x-text="plan.discretion"></p>
+          </div>
+
+          <!-- Features -->
+          <ul class="flex-1 space-y-4">
+            <template x-for="(feature, i) in plan.features" x-key="i">
+              <li class="flex items-start">
+                <svg
+                     class="w-6 h-6 text-green-300"
+                     aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 20 20"
+                     fill="currentColor"
+                     >
+                  <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clip-rule="evenodd"
+                        />
+                </svg>
+                <span class="ml-3 text-base font-medium" x-text="feature"></span>
+              </li>
+            </template>
+          </ul>
+
+          <!-- Button -->
+          <div class="flex-shrink-0 pt-4">
+            <button
+                    class="inline-flex items-center justify-center w-full max-w-xs px-4 py-2 transition-colors border rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    :class="plan.name == 'Basic' ? 'bg-indigo-500 text-white hover:bg-indigo-700' : 'hover:bg-indigo-500 hover:text-white'"
+                    x-text="`Get ${plan.name}`"
+                    ></button>
+          </div>
+        </section>
+      </template>
+    </div>
     
 
     <footer>
